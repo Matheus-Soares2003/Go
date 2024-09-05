a508e59c1e6aa78df5672e469bd150d487ca83a8
Matheus Cabral <matheusstcabral@gmail.com>
2024-09-05 11:42:04 -0300

    feat: Mapeando os comandos e flags

--- Projetos/CLI/app/app.go
+++ Projetos/CLI/app/app.go
@@ -8,5 +8,23 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e nomes de servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+
 }
