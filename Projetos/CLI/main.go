d8c8cc95a9fff70441c5246699afe03c2036b18b
Matheus Cabral <matheusstcabral@gmail.com>
2024-09-05 11:36:44 -0300

    feat: alterando main para rodar a aplicacao

--- Projetos/CLI/main.go
+++ Projetos/CLI/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"command_line/app"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("Ponto de partida")
-	app.Gerar()
+
+	aplicacao := app.Gerar()
+	if err := aplicacao.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
