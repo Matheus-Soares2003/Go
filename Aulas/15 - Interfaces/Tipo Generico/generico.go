5e38c7a6ad2ad7a3add04cc2763ea5a28acbcb32
Matheus Cabral <matheusstcabral@gmail.com>
2024-09-05 11:12:08 -0300

    Estudo sobre tipos genericos com interfaces

--- Aulas/15 - Interfaces/Tipo Generico/generico.go
+++ Aulas/15 - Interfaces/Tipo Generico/generico.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
-
+//Interfaces vazias funcionam como um parâmetro de tipo genérico (aceitam qualquer tipo)
+func generica(interf interface{}) {
+	fmt.Println(interf)
+}
 
 func main() {
-	fmt.Println("==========Métodos==========")
-	
+	fmt.Println("==========Tipos Genéricos==========")
+	generica("Olá Mundo")
+	generica(12)
+	generica(true)
+
+	mapa := map[string]interface{} {
+		"nome": "Matheus Soares",
+		"idade": 21,
+		"genero": "M",
+		"altura": 1.85,
+	}
+
+	fmt.Println(mapa)
 }
