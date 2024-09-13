ab9a7e3cf6f998e44d61d33f675d6b421e6a58cc
Matheus Cabral <matheusstcabral@gmail.com>
2024-09-13 19:46:51 -0300

    comecando o esqueleto da aplicacao

--- Exercicios/crud_simples_alunos/main.go
+++ Exercicios/crud_simples_alunos/main.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/text/cases"
 )
+
+type Aluno struct {
+	ra string
+	nome string
+	idade uint
+	curso string
+	notas []int
+}
 
 func main(){
 
-	fmt.Println("================SISTEMA DE ALUNOS================")
+	var (
+		alunos []Aluno
+		opc int
+	)
 
+	fmt.Println("================SISTEMA DE ALUNOS================")
+	for {
+
+		fmt.Println("1 - CADASTRAR ALUNO\n2 - LISTAR ALUNOS\n3 - ATUALIZAR ALUNO\n4 - DELETAR ALUNO\n5 - SAIR")
+		fmt.Print("Opção: ")
+		fmt.Scan(&opc)
+		
+		switch opc {
+		case 1:
+			//cadastrarAluno(Aluno)
+		case 2:
+			//listarAlunos()
+		case 3:
+			//atualizarAluno()
+		case 4:
+			//deletarAluno()
+		case 5:
+			break
+		default:
+			fmt.Println("OPÇÃO INVÁLIDA! Tente novamente...")
+			fmt.Println("====================================================\n\n")
+		}
+	}
 }
